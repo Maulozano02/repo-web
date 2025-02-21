d261cc03a2e5f01699aec2611ec4bcb0d5ed9dcf
Mauricio Lozano <maulozano02@gmail.com>
2025-02-20 22:37:21 -0600

    Añadi navbar y dashboard protegido final update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../App.css"; // Importamos estilos globales
+import "../App.css"; 
 
 function Login({ onLogin }) {
   const [username, setUsername] = useState("");
